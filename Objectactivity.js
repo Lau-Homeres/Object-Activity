ba9f4d116efa5eaf6d11c6e755156d7f3b26494f
Lau Homeres <laurencehomeres0@gmail.com>
2024-10-18 14:50:07 +0800

    Initial commit

---
+++ Objectactivity.js
@@ -0,0 +1,14 @@
+const car = {
+    type: "Honda",
+    model: "Civic",
+    color: "Red"
+  };
+  
+  console.log(typeof car);
+
+  car.type = "Toyota";
+  console.log(car);
+
+  car.wheels = 4;
+  console.log(car); 
+  
